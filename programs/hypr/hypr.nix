e1e07d90df9266f3a96c5c544d89d3425c15e37f
Roman Zupancic <roman.k.zupancic@gmail.com>
2023-10-07 23:00:38 -0400

    Automatic hyprpaper reload when config file changes.

--- programs/hypr/hypr.nix
+++ programs/hypr/hypr.nix
@@ -1,15 +1,22 @@
 { pkgs, ... }:
 let
-  wallpaper_path = "~/Pictures/wallpapers/evergreen-forest.png";
+  wallpaper_path = "~/Pictures/wallpapers/spaceman-gruvbox.jpg";
+  # wallpaper_path = "~/Pictures/wallpapers/city-gruvbox.png";
 in {
   # Hyprpaper config
   home.packages = with pkgs; [
     hyprpaper
   ];
-  home.file.".config/hypr/hyprpaper.conf".text = ''
+  home.file.".config/hypr/hyprpaper.conf" = {
+    text = ''
 preload = ${wallpaper_path}
 wallpaper = ,${wallpaper_path}
 '';
+    onChange = ''
+echo "Restarting hyprpaper:"
+${pkgs.killall}/bin/killall hyprpaper -q >/dev/null 2>&1 && nohup ${pkgs.hyprpaper}/bin/hyprpaper >/dev/null 2>&1 &
+'';
+      };
 
   home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf;
 }
