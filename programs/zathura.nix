99a650e8b2925b31bbc7e43fcc516f1ccebbe472
Roman Zupancic <roman.k.zupancic@gmail.com>
2023-10-09 19:46:14 -0400

    Added zathura

--- programs/zathura.nix
+++ programs/zathura.nix
@@ -1,6 +1,18 @@
-{...}:
+{ config, ...}:
 {
   programs.zathura = {
     enable = true;
+
+    options = {
+      default-fg = "#${config.colorScheme.colors.base06}";
+      default-bg = "#${config.colorScheme.colors.base00}";
+      font = "Cascadia Code";
+
+      recolor = true;
+      recolor-keephue = true;
+      recolor-darkcolor = "#${config.colorScheme.colors.base06}";
+      recolor-lightcolor = "#${config.colorScheme.colors.base00}";
+      copy-clipboard = "clipboard";
+    };
   };
 }
