23f440bbb32fbebfb5476e23aa238f21066a5433
Roman Zupancic <roman.k.zupancic@gmail.com>
2023-10-09 22:44:08 -0400

    Added credential management to git

--- programs/git.nix
+++ programs/git.nix
@@ -1,13 +1,24 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
+  home.packages = with pkgs; [
+    xdg-utils # Necessary for git-credential-manager to open a browser
+  ];
+
   programs.git = {
     enable = true;
+    package = pkgs.gitFull;
+
     userName = "Roman Zupancic";
     userEmail = "roman.k.zupancic@gmail.com";
     extraConfig = {
       init = {
         defaultBranch="main";
       };
+      credential.helper = "${pkgs.nur.repos.mloeper.git-credential-manager}/bin/git-credential-manager";
+
+      credential.credentialStore = "cache";
+
+      http.postBuffer = 157286400;
     };
   };
 }
