25659ab521e26f69b41dcb11a559771f2745795b
Roman Zupancic <roman.k.zupancic@gmail.com>
2023-10-20 19:22:46 -0400

    Hack to get eww workspaces working

--- programs/eww/barf/scripts/get-active-workspace.sh
+++ programs/eww/barf/scripts/get-active-workspace.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 # From https://wiki.hyprland.org/Useful-Utilities/Status-Bars/#configuration
 
-hyprctl monitors -j | jq '.[] | select(.focused) | .activeWorkspace.id'
-
-socat -u UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - |
-  stdbuf -o0 awk -F '>>|,' -e '/^workspace>>/ {print $2}' -e '/^focusedmon>>/ {print $3}'
+hyprctl monitors -j | jq --raw-output '.[] | select(.focused) | .activeWorkspace.id'
+socat -u UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | stdbuf -o0 awk -F '>>|,' -e '/^workspace>>/{print $2}' -e '/^focusedmon>>/{print $3}'
