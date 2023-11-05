c26fd364ad2bbfed97938a6de1700b4eabcb575d
Roman Zupancic <roman.k.zupancic@gmail.com>
2023-11-04 22:47:07 -0400

    Added ls and xdg home folders

--- home.nix
+++ home.nix
@@ -4,33 +4,61 @@
   nixpkgs.overlays = [
     inputs.nur.overlay
   ];
-
-  home.username = "romanz";
-  home.homeDirectory = "/home/romanz";
   
   home.stateVersion = "22.11";
 
   colorScheme = inputs.nix-colors.colorSchemes.gruvbox-dark-medium;
+
+  home.username = "romanz";
+  home.homeDirectory = "/home/romanz";
+  
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
   
   home.sessionVariables = {
     EDITOR = "emacsclient -c";
     PAGER = "${pkgs.most}/bin/most";
   };
 
-  
-
   home.shellAliases = {
+    # Nix Configuration
+    hc = "cd ${rootPath}";
     os-rebuild = "sudo nixos-rebuild switch --flake ${rootPath}";
     home-switch = "home-manager switch --flake ${rootPath}";
-    hc = "cd ${rootPath}";
+
+    # Because I'm always vimming
     ":q" = "exit";
+
+    # Better ls
+    ls = "${pkgs.exa}/bin/exa";
+    ll = "ls -l";
+    l = "ls -la";
+    lg = "${pkgs.exa}/bin/exa --long --header --git";
   };
-  
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
 
   # Manage XDG variables
-  xdg.enable = true;
+  xdg = {
+    enable = true;
+
+    userDirs = {
+      enable = true;
+
+      createDirectories = true;
+
+      documents = "${config.home.homeDirectory}/Documents";
+      desktop = null;
+      music = null;
+      videos = null;
+      publicShare = null;
+      templates = null;
+
+      extraConfig = {
+        XDG_PROJECTS_DIR = "${config.home.homeDirectory}/Projects";
+        XDG_SCHOOL_DIR = "${config.home.homeDirectory}/School";
+        XDG_WORK_DIR = "${config.home.homeDirectory}/Work";
+      };
+    };
+  };
 
   home.packages = with pkgs; [
     thefuck
@@ -51,5 +79,7 @@
     man-pages man-pages-posix
 
     asciiquarium
+
+    xdg-utils
   ];
 }
